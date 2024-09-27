65b5189f8dbc8abfd257ab9e1ce2f016adadee6d
tuukkaviitanen <tuukka.viitanen@gmail.com>
2024-09-27 18:57:57 +0300

    Added PUT Transaction tests

--- k6/api-tests.js
+++ k6/api-tests.js
@@ -78,6 +78,17 @@ const postTransaction = (category, description, value, timestamp, params) => {
     return http.post(`${BASE_URL}/transactions`, payload, params)
 }
 
+const putTransaction = (transactionId, category, description, value, timestamp, params) => {
+    const payload = JSON.stringify({
+        category,
+        description,
+        value,
+        timestamp
+    })
+
+    return http.put(`${BASE_URL}/transactions/${transactionId}`, payload, params)
+}
+
 export default function () {
     describe('Budget API', () => {
         describe('Credentials endpoint', () => {
@@ -97,6 +108,27 @@ export default function () {
                     expect(response.status, 'response status').to.equal(400);
                     assertValidErrorBody(response)
                 })
+
+                describe('should throw validation error on', () => {
+                    const validationErrorTestCases = [
+                        ["too short username", { username: randomString(3), password }],
+                        ["too long username", { username: randomString(51), password }],
+                        ["null username", { username: null, password }],
+                        ["too short password", { username, password: randomString(7) }],
+                        ["too long password", { username, password: randomString(51) }],
+                        ["null password", { username: null, password }],
+                    ]
+                    for (const errorTestCase of validationErrorTestCases) {
+                        const [name, parameters] = errorTestCase
+
+                        describe(name, () => {
+                            const response = postCredentials(parameters.username, parameters.password)
+
+                            expect(response.status, 'response status').to.equal(400)
+                            assertValidErrorBody(response)
+                        })
+                    }
+                })
             })
 
             describe('DELETE', () => {
@@ -105,6 +137,7 @@ export default function () {
                     expect(response.status, 'response status').to.equal(204)
                     expect(response.body, 'response status').to.be.null
                 })
+
                 describe('should respond with 401 when unable to authenticate', () => {
                     const response = deleteCredentials(username, password)
                     expect(response.status, 'response status').to.equal(401)
@@ -123,17 +156,21 @@ export default function () {
 
             const category = 'health'
             const description = "Doctor's appointment"
-            const value = 99.99
+            const value = -99.99
             const timestamp = "2024-01-01"
 
             const validationErrorTestCases = [
                 ["invalid category", { category: "invalid category", description, value, timestamp }],
+                ["null category", { category: null, description, value, timestamp }],
                 ["too short description", { category, description: randomString(3), value, timestamp }],
                 ["too long description", { category, description: randomString(201), value, timestamp }],
+                ["null description", { category, description: null, value, timestamp }],
                 ["too small value", { category, description, value: -1000000001, timestamp }],
                 ["too big value", { category, description, value: 1000000001, timestamp }],
                 ["too many decimals in value", { category, description, value: 0.001, timestamp }],
+                ["null value", { category, description, value: null, timestamp }],
                 ["invalid timestamp", { category, description, value, timestamp: "invalid timestamp" }],
+                ["null timestamp", { category, description, value, timestamp: null }],
             ]
 
             describe('POST', () => {
@@ -141,9 +178,10 @@ export default function () {
                     const response = postTransaction(category, description, value, timestamp, requestParams)
 
                     expect(response.status, 'response status').to.equal(201)
-
                     expect(response).to.have.validJsonBody();
+
                     const jsonBody = response.json()
+                    expect(jsonBody.id, "id").to.be.a("string")
                     expect(jsonBody.category, "category").to.equal(category)
                     expect(jsonBody.description, "description").to.equal(description)
                     expect(jsonBody.value, "value").to.equal(value)
@@ -162,6 +200,7 @@ export default function () {
                         })
                     }
                 })
+
                 describe('should throw authentication error on', () => {
                     describe('invalid credentials', () => {
                         const newUsername = randomString(10)
@@ -201,6 +240,87 @@ export default function () {
                         assertValidErrorBody(response)
                     })
                 })
+            })
+
+            describe('PUT', () => {
+                const postResponse = postTransaction(category, description, value, timestamp, requestParams)
+                const transactionId = postResponse.json().id
+
+                describe('should update transaction successfully', () => {
+                    const newCategory = 'recreation'
+                    const newDescription = "Ice cream"
+                    const newValue = -5
+                    const newTimestamp = "2024-06-01"
+
+                    const response = putTransaction(transactionId, newCategory, newDescription, newValue, newTimestamp, requestParams)
+
+                    expect(response.status, 'response status').to.equal(200)
+                    expect(response).to.have.validJsonBody();
+
+                    const jsonBody = response.json()
+                    expect(jsonBody.id, "category").to.equal(transactionId)
+                    expect(jsonBody.category, "category").to.equal(newCategory)
+                    expect(jsonBody.description, "description").to.equal(newDescription)
+                    expect(jsonBody.value, "value").to.equal(newValue)
+                    expect(jsonBody.timestamp, "timestamp").to.equal("2024-06-01T00:00:00.000Z")
+                })
+
+                describe('should throw validation error on', () => {
+                    for (const errorTestCase of validationErrorTestCases) {
+                        const [name, parameters] = errorTestCase
+
+                        describe(name, () => {
+                            const response = putTransaction(transactionId, parameters.category, parameters.description, parameters.value, parameters.timestamp, requestParams)
+
+                            expect(response.status, 'response status').to.equal(400)
+                            assertValidErrorBody(response)
+                        })
+                    }
+                })
+
+                describe('should throw authentication error on', () => {
+                    describe('invalid credentials', () => {
+                        const newUsername = randomString(10)
+                        const newPassword = randomString(10)
+                        const newRequestParams = getTransactionParams(newUsername, newPassword)
+
+                        const response = putTransaction(transactionId, category, description, value, timestamp, newRequestParams)
+
+                        expect(response.status, 'response status').to.equal(401)
+                        assertValidErrorBody(response)
+                    })
+
+                    describe('invalid authentication header', () => {
+                        const newRequestParams = {
+                            headers: {
+                                'Content-Type': 'application/json',
+                                'Authorization': 'invalid'
+                            },
+                        };
+
+                        const response = putTransaction(transactionId, category, description, value, timestamp, newRequestParams)
+
+                        expect(response.status, 'response status').to.equal(401)
+                        assertValidErrorBody(response)
+                    })
+
+                    describe('missing authentication header', () => {
+                        const newRequestParams = {
+                            headers: {
+                                'Content-Type': 'application/json',
+                            },
+                        };
+
+                        const response = putTransaction(transactionId, category, description, value, timestamp, newRequestParams)
+
+                        expect(response.status, 'response status').to.equal(401)
+                        assertValidErrorBody(response)
+                    })
+                })
+            })
+
+            describe('DELETE', () => {
+
             })
         })
     })
