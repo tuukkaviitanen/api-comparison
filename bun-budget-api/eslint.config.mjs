5b2b9fb63bdfdef2311e00a47b0931914546e232
tuukkaviitanen <tuukka.viitanen@gmail.com>
2024-10-01 18:15:12 +0300

    Setup eslint and prettier

---
+++ bun-budget-api/eslint.config.mjs
@@ -0,0 +1,14 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  eslintConfigPrettier, // Disables eslint formatting rules
+  eslintPluginPrettierRecommended, // Adds prettier formatting rules to eslint
+  ...tseslint.configs.recommended,
+];
--- bun-budget-api/src/index.ts
+++ bun-budget-api/src/index.ts
@@ -1,9 +1,9 @@
 import { Elysia } from "elysia";
 
-const PORT = Number(process.env.PORT) || 8080
+const PORT = Number(process.env.PORT) || 8080;
 
 const app = new Elysia().get("/", () => "Hello Elysia").listen(PORT);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
