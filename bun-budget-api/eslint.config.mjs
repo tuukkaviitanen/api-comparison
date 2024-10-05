42f6673af8d0f57590b7b825383bce8b130686a8
tuukkaviitanen <tuukka.viitanen@gmail.com>
2024-10-05 11:00:27 +0300

    Set ESLint ignore on prisma and out

--- bun-budget-api/eslint.config.mjs
+++ bun-budget-api/eslint.config.mjs
@@ -5,10 +5,13 @@ import eslintConfigPrettier from "eslint-config-prettier";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { files: ["**/*.{ts}"] },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   eslintConfigPrettier, // Disables eslint formatting rules
   eslintPluginPrettierRecommended, // Adds prettier formatting rules to eslint
   ...tseslint.configs.recommended,
+  {
+    ignores: ["**/out", "**/prisma/client"],
+  },
 ];
