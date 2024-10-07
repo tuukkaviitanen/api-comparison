91eedeb401225ed45284e969e5bc8d19ef7786e3
Tuukka Viitanen <97726090+tuukkaviitanen@users.noreply.github.com>
2024-10-07 19:33:34 +0300

    Created dummy endpoints and setup static openapi file serving (#59)

--- cs-budget-api/main/src/Program.cs
+++ cs-budget-api/main/src/Program.cs
@@ -1,8 +1,19 @@
+using Routers;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 var PORT = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 
-app.MapGet("/", () => "Hello World!");
+app.MapTransactionRouter();
+app.MapCredentialRouter();
+app.MapReportRouter();
+
+app.MapGet("/openapi.yaml", async (context) =>
+{
+    var filePath = Path.Combine(builder.Environment.ContentRootPath, "openapi.yaml");
+    context.Response.ContentType = "text/yaml";
+    await context.Response.SendFileAsync(filePath);
+});
 
 app.Run($"http://*:{PORT}");
