ecbc056bbfa358a074a8efcbcf1326278afd6bce
Tuukka Viitanen <97726090+tuukkaviitanen@users.noreply.github.com>
2024-10-07 18:05:27 +0300

    Setup C# project with development environment (#58)

---
+++ cs-budget-api/main/src/Program.cs
@@ -0,0 +1,8 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+var PORT = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run($"http://*:{PORT}");
