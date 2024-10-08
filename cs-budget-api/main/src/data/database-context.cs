3d9ecdc48591ebf6622e535b48a64365a59e2e8b
tuukkaviitanen <tuukka.viitanen@gmail.com>
2024-10-08 11:52:19 +0300

    Added some additional migration setup

--- cs-budget-api/main/src/data/database-context.cs
+++ cs-budget-api/main/src/data/database-context.cs
@@ -8,4 +8,34 @@ public class DatabaseContext(DbContextOptions options) : DbContext(options)
     public required DbSet<Credential> Credentials { get; set; }
     public required DbSet<Transaction> Transactions { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Credentials table mapping
+        modelBuilder.Entity<Credential>()
+            .Property(credentail => credentail.Username)
+            .IsRequired()
+            .HasMaxLength(50);
+        modelBuilder.Entity<Credential>()
+            .HasIndex(credential => credential.Username).IsUnique();
+        modelBuilder.Entity<Credential>()
+            .Property(credential => credential.PasswordHash)
+            .IsRequired()
+            .HasMaxLength(64);
+
+        // Transactions table mapping
+        modelBuilder.Entity<Transaction>()
+            .Property(transaction => transaction.Category)
+            .IsRequired()
+            .HasMaxLength(50);
+        modelBuilder.Entity<Transaction>()
+            .Property(transaction => transaction.Description)
+            .IsRequired()
+            .HasMaxLength(200);
+        modelBuilder.Entity<Transaction>()
+            .Property(transaction => transaction.Value)
+            .HasColumnType("DECIMAL");
+        modelBuilder.Entity<Transaction>()
+            .Property(transaction => transaction.Timestamp)
+            .HasPrecision(3);
+    }
 }
