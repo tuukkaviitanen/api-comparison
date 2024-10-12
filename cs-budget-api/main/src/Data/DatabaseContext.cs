ae7fe1e7fccc0006639cd01c630e64e1bfc1dd4f
tuukkaviitanen <tuukka.viitanen@gmail.com>
2024-10-12 15:42:11 +0300

    Fixed formatting errors

--- cs-budget-api/main/src/Data/DatabaseContext.cs
+++ cs-budget-api/main/src/Data/DatabaseContext.cs
@@ -8,7 +8,7 @@ public class DatabaseContext : DbContext
     public virtual required DbSet<Credential> Credentials { get; set; }
     public virtual required DbSet<Transaction> Transactions { get; set; }
 
-    public DatabaseContext(){}
+    public DatabaseContext() { }
 
-    public DatabaseContext(DbContextOptions options) : base(options) {}
+    public DatabaseContext(DbContextOptions options) : base(options) { }
 }
