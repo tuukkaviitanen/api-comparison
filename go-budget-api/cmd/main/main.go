b86b8684ed92d7994786d8114cf33050b59114c0
tuukkaviitanen <tuukka.viitanen@gmail.com>
2024-10-16 18:32:26 +0300

    Added GORM packages and refactored routers

--- go-budget-api/cmd/main/main.go
+++ go-budget-api/cmd/main/main.go
@@ -5,24 +5,13 @@ import (
 	"fmt"
 	"log"
 	"os"
-
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	PORT := os.Getenv("PORT")
 	address := fmt.Sprintf(":%s", PORT)
 
-	router := gin.Default()
-
-	routers.MapTransactionRouter(router)
-	routers.MapCredentialsRouter(router)
-	routers.MapReportRouter(router)
-
-	router.GET("/openapi.yaml", func(c *gin.Context) {
-		c.Header("Content-Type", "text/yaml")
-		c.File("openapi.yaml")
-	})
+	router := routers.GetMainRouter()
 
 	if err := router.Run(address); err != nil {
 		log.Fatalln("Error occurred while running the server, Error", err)
