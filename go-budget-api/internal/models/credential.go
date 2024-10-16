f976bb92b7d9a79339de0a595c658119c8ecb378
tuukkaviitanen <tuukka.viitanen@gmail.com>
2024-10-16 22:04:41 +0300

    Fixed formatting errors

--- go-budget-api/internal/models/credential.go
+++ go-budget-api/internal/models/credential.go
@@ -6,12 +6,12 @@ import (
 )
 
 type Credential struct {
-	Id uuid.UUID `gorm:"type:uuid;primary_key"`
-	Username string `gorm:"type:varchar(50);unique;NOT NULL"`
-	Password string `gorm:"type:varchar(64);NOT NULL"`
+	Id       uuid.UUID `gorm:"type:uuid;primary_key"`
+	Username string    `gorm:"type:varchar(50);unique;NOT NULL"`
+	Password string    `gorm:"type:varchar(64);NOT NULL"`
 }
 
 func (c *Credential) BeforeCreate(tx *gorm.DB) (err error) {
 	c.Id = uuid.New()
 	return
-   }
+}
--- go-budget-api/internal/models/transaction.go
+++ go-budget-api/internal/models/transaction.go
@@ -8,16 +8,16 @@ import (
 )
 
 type Transaction struct {
-	Id uuid.UUID `gorm:"type:uuid;primary_key"`
-	Category string `gorm:"type:varchar(50);NOT NULL"`
-	Description string `gorm:"type:varchar(200);NOT NULL"`
-	Value float32 `gorm:"type:decimal;NOT NULL"`
-	Timestamp time.Time `gorm:"type:timestamp(3);NOT NULL"`
-	Credential Credential `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
-	CredentialId uuid.UUID `gorm:"NOT NULL"`
+	Id           uuid.UUID  `gorm:"type:uuid;primary_key"`
+	Category     string     `gorm:"type:varchar(50);NOT NULL"`
+	Description  string     `gorm:"type:varchar(200);NOT NULL"`
+	Value        float32    `gorm:"type:decimal;NOT NULL"`
+	Timestamp    time.Time  `gorm:"type:timestamp(3);NOT NULL"`
+	Credential   Credential `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	CredentialId uuid.UUID  `gorm:"NOT NULL"`
 }
 
 func (t *Transaction) BeforeCreate(tx *gorm.DB) (err error) {
 	t.Id = uuid.New()
 	return
-   }
+}
