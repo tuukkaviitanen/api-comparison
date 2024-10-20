c214066308c725b21ee1fc42d663db46b422c199
tuukkaviitanen <tuukka.viitanen@gmail.com>
2024-10-20 18:38:44 +0300

    Added static file serving

--- rust-budget-api/src/router.rs
+++ rust-budget-api/src/router.rs
@@ -3,10 +3,12 @@ mod report_router;
 mod transaction_router;
 
 use axum::Router;
+use tower_http::services::ServeFile;
 
 pub fn app() -> Router {
     return Router::new()
         .nest("/credentials", credential_router::routes())
         .nest("/reports", report_router::routes())
-        .nest("/transactions", transaction_router::routes());
+        .nest("/transactions", transaction_router::routes())
+        .nest_service("/openapi.yaml", ServeFile::new("./openapi.yaml"));
 }
