be1e32db0ee44fa136bf397ddc0fefcf66d8cd24
tuukkaviitanen <tuukka.viitanen@gmail.com>
2024-10-21 18:37:05 +0300

    Fixed formatting errors

--- rust-budget-api/src/schema.rs
+++ rust-budget-api/src/schema.rs
@@ -25,7 +25,4 @@ diesel::table! {
 
 diesel::joinable!(transactions -> credentials (credential_id));
 
-diesel::allow_tables_to_appear_in_same_query!(
-    credentials,
-    transactions,
-);
+diesel::allow_tables_to_appear_in_same_query!(credentials, transactions,);
