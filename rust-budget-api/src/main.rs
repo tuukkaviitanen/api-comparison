5b748a194365f09715dbaba3b55b8a497e98d3d2
tuukkaviitanen <tuukka.viitanen@gmail.com>
2024-10-21 16:09:59 +0300

    Added database connection setup in main

--- rust-budget-api/src/main.rs
+++ rust-budget-api/src/main.rs
@@ -1,12 +1,23 @@
 mod router;
+use std::env;
 
+use diesel::{Connection, PgConnection};
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let port = env::var("PORT").unwrap_or(String::from("8080"));
+
+    PgConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url));
+
     let app = router::app();
 
-    let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    let address = format!("0.0.0.0:{}", port);
 
+    println!("Starting listening to {}", address);
+
+    let listener = TcpListener::bind(address).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
