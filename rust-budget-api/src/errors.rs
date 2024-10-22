8631d135583ba0e0f3a8a4fba396821820792cbc
tuukkaviitanen <tuukka.viitanen@gmail.com>
2024-10-22 21:44:59 +0300

    Added proper error status codes

--- rust-budget-api/src/errors.rs
+++ rust-budget-api/src/errors.rs
@@ -20,32 +20,45 @@ pub enum Error {
 
 impl IntoResponse for Error {
     fn into_response(self) -> Response {
-        (
-            StatusCode::INTERNAL_SERVER_ERROR,
-            Json(json!({"error": parse_error_message(self)})),
-        )
-            .into_response()
+        let (status_code, error_message) = parse_error(self);
+
+        (status_code, Json(json!({"error": error_message}))).into_response()
     }
 }
 
-fn parse_error_message(error: Error) -> String {
+fn parse_error(error: Error) -> (StatusCode, String) {
     use Error::*;
 
     const AUTH_ERROR_PREFIX: &str = "Authentication error:";
 
     match error {
-        AuthHeaderInvalidBase64 => format!("{} Invalid base64", AUTH_ERROR_PREFIX),
-        AuthHeaderMissingError => {
-            format!("{} Authorization header missing", AUTH_ERROR_PREFIX)
-        }
-        AuthHeaderWrongFormat => {
-            format!("{} Authorization header wrong format", AUTH_ERROR_PREFIX)
-        }
-        AuthInvalidCredentials => format!("{} Invalid credentials", AUTH_ERROR_PREFIX),
-        AuthInvalidCredentialsFormat => {
-            format!("{} Invalid credentials format", AUTH_ERROR_PREFIX)
-        }
-        UnexpectedError => format!("Unexpected error occurred"),
-        UniqueError => format!("Unique constraint error occurred"),
+        AuthHeaderInvalidBase64 => (
+            StatusCode::UNAUTHORIZED,
+            format!("{} Invalid base64", AUTH_ERROR_PREFIX),
+        ),
+        AuthHeaderMissingError => (
+            StatusCode::UNAUTHORIZED,
+            format!("{} Authorization header missing", AUTH_ERROR_PREFIX),
+        ),
+        AuthHeaderWrongFormat => (
+            StatusCode::UNAUTHORIZED,
+            format!("{} Authorization header wrong format", AUTH_ERROR_PREFIX),
+        ),
+        AuthInvalidCredentials => (
+            StatusCode::UNAUTHORIZED,
+            format!("{} Invalid credentials", AUTH_ERROR_PREFIX),
+        ),
+        AuthInvalidCredentialsFormat => (
+            StatusCode::BAD_REQUEST,
+            format!("{} Invalid credentials format", AUTH_ERROR_PREFIX),
+        ),
+        UniqueError => (
+            StatusCode::BAD_REQUEST,
+            format!("Unique constraint error occurred"),
+        ),
+        UnexpectedError => (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Unexpected error occurred"),
+        ),
     }
 }
