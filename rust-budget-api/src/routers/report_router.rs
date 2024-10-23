29c3f65ea5be94e7a2f366b19e118d06be4500c8
tuukkaviitanen <tuukka.viitanen@gmail.com>
2024-10-23 19:04:38 +0300

    Added GET report

--- rust-budget-api/src/routers/report_router.rs
+++ rust-budget-api/src/routers/report_router.rs
@@ -1,9 +1,41 @@
-use axum::{http::StatusCode, middleware, routing::get, Router};
+use axum::{
+    debug_handler,
+    extract::Query,
+    http::StatusCode,
+    middleware,
+    response::{IntoResponse, Response},
+    routing::get,
+    Extension, Json, Router,
+};
+use chrono::{DateTime, Utc};
+use serde::Deserialize;
+use uuid::Uuid;
 
-use crate::middlewares;
+use crate::{errors::Error, middlewares, services::report_service};
 
 pub fn routes() -> Router {
     Router::new()
-        .route("/", get(|| async { StatusCode::OK }))
+        .route("/", get(get_report))
         .route_layer(middleware::from_fn(middlewares::authenticate))
+}
+
+#[derive(Deserialize)]
+struct ReportQueryParams {
+    category: Option<String>,
+    from: Option<DateTime<Utc>>,
+    to: Option<DateTime<Utc>>,
+}
+
+#[debug_handler]
+async fn get_report(
+    Extension(credential_id): Extension<Uuid>,
+    Query(query): Query<ReportQueryParams>,
+) -> Result<Response, Error> {
+    let from_naive = query.from.map(|dt| dt.naive_utc());
+    let to_naive = query.to.map(|dt| dt.naive_utc());
+
+    let report = report_service::get_report(credential_id, query.category, from_naive, to_naive)
+        .map_err(|_| Error::Unexpected)?;
+
+    Ok((StatusCode::OK, Json(report)).into_response())
 }
--- rust-budget-api/src/services.rs
+++ rust-budget-api/src/services.rs
@@ -1,3 +1,4 @@
 pub mod credentials_service;
 pub mod errors;
+pub mod report_service;
 pub mod transaction_service;
