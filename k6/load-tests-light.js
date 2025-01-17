8b358f74b191b292b886e3a887309496f8375acb
tuukkaviitanen <tuukka.viitanen@gmail.com>
2025-01-17 19:55:07 +0200

    Updated light tests and optimized rust db connections

--- k6/load-tests-light.js
+++ k6/load-tests-light.js
@@ -2,6 +2,7 @@ import { expect } from "https://jslib.k6.io/k6chaijs/4.3.4.3/index.js";
 import { randomString } from "https://jslib.k6.io/k6-utils/1.4.0/index.js";
 import {
   deleteCredentials,
+  getOpenAPI,
   getReport,
   getTransactionParams,
   getTransactions,
@@ -39,49 +40,7 @@ export const handleSummary = (data) => {
 };
 
 export default () => {
-  const username = randomString(10);
-  const password = randomString(10);
+  const response = getOpenAPI();
 
-  // POST Credential
-  const postCredentialsResponse = postCredentials(username, password);
-
-  expect(
-    postCredentialsResponse.status,
-    "POST credential response status"
-  ).to.equal(204);
-
-  const transactionParams = getTransactionParams(username, password);
-
-  // POST Transaction
-  const postResponse = postTransaction(
-    "Food & Drinks",
-    "Pizza at Frank's",
-    -10.99,
-    "2024-01-01T18:00:00Z",
-    transactionParams
-  );
-
-  expect(postResponse.status, "POST transaction response status").to.equal(201);
-
-  // GET Transactions
-  const getResponse = getTransactions(
-    undefined,
-    undefined,
-    undefined,
-    undefined,
-    undefined,
-    undefined,
-    undefined,
-    transactionParams
-  );
-
-  expect(getResponse.status, "GET transactions response status").to.equal(200);
-
-  // DELETE the credential
-  const deleteCredentialResponse = deleteCredentials(username, password);
-
-  expect(
-    deleteCredentialResponse.status,
-    "DELETE credential response status"
-  ).to.equal(204);
+  expect(response.status, "response status").to.equal(200);
 };
--- rust-budget-api/src/database.rs
+++ rust-budget-api/src/database.rs
@@ -5,36 +5,29 @@ use diesel::{
     PgConnection,
 };
 use errors::DatabaseError;
-use lazy_static::lazy_static;
-use std::sync::Mutex;
+use std::sync::Arc;
+use tokio::sync::OnceCell;
 
 pub type DbConnectionPool = Pool<ConnectionManager<PgConnection>>;
 pub type DbConnection = PooledConnection<ConnectionManager<PgConnection>>;
 
-lazy_static! {
-    static ref CONNECTION_POOL: Mutex<Option<DbConnectionPool>> = Mutex::new(None);
-}
+static CONNECTION_POOL: OnceCell<Arc<DbConnectionPool>> = OnceCell::const_new();
 
 pub fn initialize_connection_pool(database_url: String) {
     let manager = ConnectionManager::<PgConnection>::new(database_url);
     let pool = Pool::builder()
         .max_size(100)
+        .min_idle(Some(5))
         .test_on_check_out(true)
         .build(manager)
         .expect("Could not build database connection pool");
 
-    let mut context = CONNECTION_POOL.lock().unwrap();
-    *context = Some(pool);
+    CONNECTION_POOL.set(Arc::new(pool)).unwrap();
 }
 
 pub fn get_connection() -> Result<DbConnection, DatabaseError> {
     let pool = CONNECTION_POOL
-        .lock()
-        .map_err(|_| DatabaseError::PoolMutexLockFailed)?;
-
-    if let Some(pool) = &*pool {
-        pool.get().map_err(|_| DatabaseError::ConnectionFailed)
-    } else {
-        Err(DatabaseError::PoolUninitialized)
-    }
+        .get()
+        .ok_or(DatabaseError::PoolUninitialized)?;
+    pool.get().map_err(|_| DatabaseError::ConnectionFailed)
 }
